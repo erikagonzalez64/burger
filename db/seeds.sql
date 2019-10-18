0653f9937e0f34cb015e46679b9b5d2d7bf18ca1
erika gonzalez <gonzalezerika75951@gmail.com>
2019-10-17 23:11:12 -0500

    added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+SELECT * FROM burgers_db.burgers;
+
+INSERT INTO burgers (burger_name) VALUES
+("Ultra bacon with eggs"),
+("Fried onions with provologne cheese");
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+("Turkey burger w/ bell peppers", true);
